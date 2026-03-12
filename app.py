f405961c19fac7220d46628fff64d4d39d702971
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2026-03-12 12:32:58 +0530

    model connected to flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,render_template
+from src.predict import predict_sentiment
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    review = request.form['review']
+
+    result = predict_sentiment(review)
+
+    return render_template(
+        "index.html",
+        prediction_text=result
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/predict.py
+++ src/predict.py
@@ -2,15 +2,19 @@ import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-model = load_model("models/sentiment_model.h5")
+# Load model
+model = load_model(r"C:\Users\anant\Desktop\sentiment_analysis\src\models\sentiment_model.h5")
 
-tokenizer = pickle.load(open("models/tokenizer.pkl","rb"))
+# Load tokenizer
+with open(r"C:\Users\anant\Desktop\sentiment_analysis\src\models\tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
 
 def predict_sentiment(text):
 
     seq = tokenizer.texts_to_sequences([text])
 
-    padded = pad_sequences(seq,maxlen=200)
+    padded = pad_sequences(seq, maxlen=200)
 
     pred = model.predict(padded)
 
